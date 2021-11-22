38714bd155dbcb7c4372e1607022252f339dff56
CarlosR <carlosromero.biz@gmail.com>
2021-11-21 21:13:33 -0700

    change settings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList'
-import uuidv4 from "uuid/v4";
+import uuidv4 from 'uuid/v4'
 
 const LOCAL_STORAGE_KEY = 'todoApp.todos';
 
